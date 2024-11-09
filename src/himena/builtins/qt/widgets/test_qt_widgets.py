dc213f56f49e9a8b79dfaa7b423f9f084801b576
Hanjin Liu <liuhanjin-sc@i.softbank.jp>
2024-11-09 18:35:00 +0900

    more tests

---
+++ src/himena/builtins/qt/widgets/test_qt_widgets.py
@@ -0,0 +1,110 @@
+import numpy as np
+from qtpy.QtCore import Qt
+from himena.builtins.qt.widgets import (
+    QDefaultTextEdit,
+    QDefaultTableWidget,
+    QDefaultImageView,
+)
+from himena import WidgetDataModel
+from pytestqt.qtbot import QtBot
+
+
+def test_text_edit(qtbot: QtBot):
+    model = WidgetDataModel(value="a\nb", type="text")
+    text_edit = QDefaultTextEdit.from_model(model)
+    qtbot.addWidget(text_edit)
+    main = text_edit._main_text_edit
+
+    assert text_edit.to_model().value == "a\nb"
+    assert text_edit.toPlainText() == "a\nb"
+    qtbot.keyClick(main, Qt.Key.Key_End, modifier=Qt.KeyboardModifier.ControlModifier)
+    qtbot.keyClick(main, Qt.Key.Key_Return)
+    qtbot.keyClick(main, Qt.Key.Key_Tab)
+    qtbot.keyClick(main, Qt.Key.Key_Backtab)
+    qtbot.keyClick(main, Qt.Key.Key_Tab)
+    qtbot.keyClick(main, Qt.Key.Key_O)
+    qtbot.keyClick(main, Qt.Key.Key_P)
+    assert text_edit.to_model().value.splitlines()[-1] == "    op"
+    qtbot.keyClick(main, Qt.Key.Key_Home)
+    qtbot.keyClick(main, Qt.Key.Key_Up, modifier=Qt.KeyboardModifier.AltModifier)
+    qtbot.keyClick(main, Qt.Key.Key_Down, modifier=Qt.KeyboardModifier.AltModifier)
+    qtbot.keyClick(main, Qt.Key.Key_Down)
+    qtbot.keyClick(main, Qt.Key.Key_Down)
+    qtbot.keyClick(main, Qt.Key.Key_Down)
+    qtbot.keyClick(main, Qt.Key.Key_Tab)
+    qtbot.keyClick(main, Qt.Key.Key_X)
+    qtbot.keyClick(main, Qt.Key.Key_Return)
+    qtbot.keyClick(main, Qt.Key.Key_A)
+    qtbot.keyClick(main, Qt.Key.Key_B)
+    qtbot.keyClick(main, Qt.Key.Key_C)
+    qtbot.keyClick(main, Qt.Key.Key_D)
+    qtbot.keyClick(main, Qt.Key.Key_L, modifier=Qt.KeyboardModifier.ControlModifier)
+    qtbot.keyClick(main, Qt.Key.Key_Up, modifier=Qt.KeyboardModifier.AltModifier)
+    qtbot.keyClick(main, Qt.Key.Key_Down, modifier=Qt.KeyboardModifier.AltModifier)
+    qtbot.keyClick(main, Qt.Key.Key_Left)
+    qtbot.keyClick(main, Qt.Key.Key_D, modifier=Qt.KeyboardModifier.ControlModifier)
+    qtbot.keyClick(main, Qt.Key.Key_C, modifier=Qt.KeyboardModifier.ControlModifier)
+    qtbot.keyClick(main, Qt.Key.Key_Return)
+    qtbot.keyClick(main, Qt.Key.Key_V, modifier=Qt.KeyboardModifier.ControlModifier)
+    qtbot.keyClick(main, Qt.Key.Key_Less, modifier=Qt.KeyboardModifier.ControlModifier)
+    qtbot.keyClick(
+        main, Qt.Key.Key_Greater, modifier=Qt.KeyboardModifier.ControlModifier
+    )
+    qtbot.keyClick(
+        main, Qt.Key.Key_Greater, modifier=Qt.KeyboardModifier.ControlModifier
+    )
+    qtbot.keyClick(main, Qt.Key.Key_0, modifier=Qt.KeyboardModifier.ControlModifier)
+    qtbot.keyClick(
+        text_edit, Qt.Key.Key_F, modifier=Qt.KeyboardModifier.ControlModifier
+    )
+    text_edit.resize(100, 100)
+    text_edit.resize(120, 120)
+
+
+def test_table_edit(qtbot: QtBot):
+    model = WidgetDataModel(value=[["a", "b"], [0, 1]], type="table")
+    table_widget = QDefaultTableWidget.from_model(model)
+    qtbot.addWidget(table_widget)
+    qtbot.keyClick(
+        table_widget, Qt.Key.Key_A, modifier=Qt.KeyboardModifier.ControlModifier
+    )
+    qtbot.keyClick(
+        table_widget, Qt.Key.Key_C, modifier=Qt.KeyboardModifier.ControlModifier
+    )
+    qtbot.keyClick(
+        table_widget, Qt.Key.Key_X, modifier=Qt.KeyboardModifier.ControlModifier
+    )
+    qtbot.keyClick(
+        table_widget, Qt.Key.Key_V, modifier=Qt.KeyboardModifier.ControlModifier
+    )
+    qtbot.keyClick(table_widget, Qt.Key.Key_Delete)
+    qtbot.keyClick(
+        table_widget, Qt.Key.Key_F, modifier=Qt.KeyboardModifier.ControlModifier
+    )
+    table_widget.resize(100, 100)
+    qtbot
+
+
+def test_image_view(qtbot: QtBot):
+    # grayscale
+    model = WidgetDataModel(
+        value=np.arange(100, dtype=np.uint8).reshape(10, 10), type="image"
+    )
+    image_view = QDefaultImageView.from_model(model)
+    assert len(image_view._sliders) == 0
+
+    # RGB
+    model = WidgetDataModel(
+        value=np.zeros((100, 100, 3), dtype=np.uint16), type="image"
+    )
+    image_view = QDefaultImageView.from_model(model)
+    assert len(image_view._sliders) == 0
+    image_view._interpolation_check_box.setChecked(False)
+    image_view._interpolation_check_box.setChecked(True)
+
+    # 5D
+    rng = np.random.default_rng(14442)
+    model = WidgetDataModel(
+        value=rng.random((10, 5, 3, 100, 100), dtype=np.float32), type="image"
+    )
+    image_view = QDefaultImageView.from_model(model)
