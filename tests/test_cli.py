96a7ffe4a1d1bb353bd1da44348f90ed2eb5fc19
Hanjin Liu <liuhanjin-sc@i.softbank.jp>
2025-03-25 09:06:57 +0900

    update test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 import sys
+import pytest
 from himena.__main__ import main
 
 def test_simple():
@@ -10,16 +11,27 @@ PROF_NAME = "test"
 def test_new_profile():
     sys.argv = ["himena", "--new", PROF_NAME]
     main()
+    with pytest.raises(ValueError):
+        sys.argv = ["himena", PROF_NAME, "--new", "xyz"]
+        main()
+    sys.argv = ["himena", "--remove", PROF_NAME]
+    main()
 
 def test_list_plugins():
+    sys.argv = ["himena", "--new", PROF_NAME]
+    main()
     sys.argv = ["himena", "--list-plugins"]
     main()
     sys.argv = ["himena", PROF_NAME, "--list-plugins"]
     main()
     sys.argv = ["himena", PROF_NAME, "--install", "himena_builtins"]
+    main()
     sys.argv = ["himena", PROF_NAME, "--install", "himena_builtins.io"]
+    main()
     sys.argv = ["himena", PROF_NAME, "--uninstall", "himena_builtins"]
+    main()
     sys.argv = ["himena", PROF_NAME, "--uninstall", "himena_builtins.qt.widgets"]
+    main()
 
 def test_install_uninstall():
     sys.argv = ["himena", "--uninstall", "himena-builtins"]
