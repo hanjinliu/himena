74ae38712c60520efaa5631e18d21a307b6f5f3b
Hanjin Liu <liuhanjin-sc@i.softbank.jp>
2025-12-08 20:07:58 +0000

    skip test for qt5

--- src/himena_builtins/_tests/test_pdf.py
+++ src/himena_builtins/_tests/test_pdf.py
@@ -2,12 +2,13 @@ from pathlib import Path
 import pytest
 from pytestqt.qtbot import QtBot
 from himena import MainWindow
-from himena_builtins.qt.widgets.pdf import QPdfViewer
 from qtpy.QtCore import Qt
 from qtpy import QT5
 
 @pytest.mark.skipif(QT5, reason="Qt5 does not implement QtPdfWidgets.")
 def test_pdf_viewer(himena_ui: MainWindow, qtbot: QtBot, sample_dir: Path):
+    from himena_builtins.qt.widgets.pdf import QPdfViewer
+
     himena_ui.read_file(sample_dir / "pdf.pdf")
     widget = himena_ui.current_window.widget
     assert isinstance(widget, QPdfViewer)
